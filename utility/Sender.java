b1ad167586152d6214e0117777a9688ec7d99837
UtkarshRawat1710 <124810125+UtkarshRawat1710@users.noreply.github.com>
2023-07-29 13:29:42 +0530

    Sender-Module

---
+++ utility/Sender.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sanjeevaniapp.utility;
+
+/**
+ *
+ * @author pc
+ */
+public interface Sender {
+   boolean send(String number,String data)throws Exception;
+}
